ca8e3fe5466bb7a32207b9353f3f0f2f2eff206d
Miguel Usma <usmamiguel04@gmail.com>
2024-04-13 10:25:18 -0500

    AlertService

---
+++ LabScript/AlertService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+
+namespace LabScript
+{
+    public class AlertService : IAsyncDisposable
+    {
+        readonly Lazy<Task<IJSObjectReference>> ijsObjectReference;
+
+        public AlertService(IJSRuntime ijsRuntime)
+        {
+            this.ijsObjectReference = new Lazy<Task<IJSObjectReference>>(() =>
+            ijsRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/Home.razor.js").AsTask());
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (ijsObjectReference.IsValueCreated)
+            {
+                IJSObjectReference moduleJs = await ijsObjectReference.Value;
+                await moduleJs.DisposeAsync();
+            }
+        }
+    }
+}
